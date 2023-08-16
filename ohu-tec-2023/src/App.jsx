bd7af62d98c23c6202690d09d6175dffe2381ee3
randy-25 <randyrevaldo64@gmail.com>
2023-08-16 18:49:30 +0700

    Add Kegiatan.jsx & RegistrationInfo.jsx

---
+++ ohu-tec-2023/src/App.jsx
@@ -0,0 +1,13 @@
+import RegistrationInfo from "./component/RegistrationInfo.jsx"
+import Kegiatan from "./component/Kegiatan.jsx"
+
+function App() {
+  return (
+    <div className="App">
+      <Kegiatan />
+      <RegistrationInfo />
+    </div>
+  );
+}
+
+export default App;
