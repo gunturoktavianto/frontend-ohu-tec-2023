f679a02f206e3a14f0330606c63bf17fa99944b7
Vincentius7704 <vincentiusbhatara04@gmail.com>
2023-08-17 13:54:15 +0700

    Price List

--- ohu-tec-2023/src/App.jsx
+++ ohu-tec-2023/src/App.jsx
@@ -1,10 +1,12 @@
 import RegistrationInfo from "./component/RegistrationInfo.jsx"
 import Kegiatan from "./component/Kegiatan.jsx"
-
+import PriceList from "./component/PriceList.jsx";
 function App() {
   return (
     <div className="App">
       <Kegiatan />
+      <RegistrationInfo />
+      <PriceList></PriceList>
       <RegistrationInfo />
     </div>
   );
