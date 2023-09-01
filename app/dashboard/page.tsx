676eb23482ebe3ba8a42faecfc1b4e75afaad13a
Vincentius7704 <vincentiusbhatara04@gmail.com>
2023-09-01 22:47:48 +0700

    Dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,5 +1,6 @@
 import React from "react";
+import Dashboard from "../component-ohu/Dashboard";
 
 export default function Registration() {
-    return <div>Registration</div>;
+    return <Dashboard></Dashboard>;
 }
