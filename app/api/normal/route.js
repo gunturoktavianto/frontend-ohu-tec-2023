a5822745a949067cb43a2ddfc0088dc6fd5c88d5
Guntur Oktavianto Nugroho <gunturoktaviant@gmail.com>
2023-09-03 09:44:19 +0700

    uncomment api

--- app/api/normal/route.js
+++ app/api/normal/route.js
@@ -1,45 +1,45 @@
-// import connectDB from "@/lib/mongodb";
-// import Normal from "@/app/models/normal";
-// import { NextResponse } from "next/server";
-// import mongoose from "mongoose";
+import connectDB from "@/lib/mongodb";
+import Normal from "@/app/models/normal";
+import { NextResponse } from "next/server";
+import mongoose from "mongoose";
 
-// export async function POST(req) {
-//     const {
-//         emailNormal,
-//         namaNormal,
-//         nimNormal,
-//         fakultasNormal,
-//         jurusanNormal,
-//         idLineNormal,
-//         paket,
-//         isPerwakilan,
-//         namaPerwakilan,
-//         buktiBayarNormal,
-//         buktiPersyaratanNormal,
-//     } = await req.json();
+export async function POST(req) {
+    const {
+        emailNormal,
+        namaNormal,
+        nimNormal,
+        fakultasNormal,
+        jurusanNormal,
+        idLineNormal,
+        paket,
+        isPerwakilan,
+        namaPerwakilan,
+        buktiBayarNormal,
+        buktiPersyaratanNormal,
+    } = await req.json();
 
-//     try {
-//         await connectDB();
-//         await Normal.create({
-//             emailNormal,
-//             namaNormal,
-//             nimNormal,
-//             fakultasNormal,
-//             jurusanNormal,
-//             idLineNormal,
-//             paket,
-//             isPerwakilan,
-//             namaPerwakilan,
-//             buktiBayarNormal,
-//             buktiPersyaratanNormal,
-//         });
-//         return NextResponse.json({
-//             msg: ["Registration Sucessful."],
-//             success: true,
-//         });
-//     } catch (error) {
-//         if (error instanceof mongoose.Error.ValidationError) {
-//         } else {
-//         }
-//     }
-// }
+    try {
+        await connectDB();
+        await Normal.create({
+            emailNormal,
+            namaNormal,
+            nimNormal,
+            fakultasNormal,
+            jurusanNormal,
+            idLineNormal,
+            paket,
+            isPerwakilan,
+            namaPerwakilan,
+            buktiBayarNormal,
+            buktiPersyaratanNormal,
+        });
+        return NextResponse.json({
+            msg: ["Registration Sucessful."],
+            success: true,
+        });
+    } catch (error) {
+        if (error instanceof mongoose.Error.ValidationError) {
+        } else {
+        }
+    }
+}
--- app/models/normal.js
+++ app/models/normal.js
@@ -1,62 +1,62 @@
-// import mongoose, { Schema } from "mongoose";
+import mongoose, { Schema } from "mongoose";
 
-// const normalSchema = new Schema({
-//     emailNormal: {
-//         type: String,
-//         required: true,
-//     },
+const normalSchema = new Schema({
+    emailNormal: {
+        type: String,
+        required: true,
+    },
 
-//     namaNormal: {
-//         type: String,
-//         required: true,
-//     },
+    namaNormal: {
+        type: String,
+        required: true,
+    },
 
-//     nimNormal: {
-//         type: String,
-//         required: true,
-//     },
+    nimNormal: {
+        type: String,
+        required: true,
+    },
 
-//     fakultasNormal: {
-//         type: String,
-//         required: true,
-//     },
+    fakultasNormal: {
+        type: String,
+        required: true,
+    },
 
-//     jurusanNormal: {
-//         type: String,
-//         required: true,
-//     },
+    jurusanNormal: {
+        type: String,
+        required: true,
+    },
 
-//     idLineNormal: {
-//         type: String,
-//         required: true,
-//     },
+    idLineNormal: {
+        type: String,
+        required: true,
+    },
 
-//     paket: {
-//         required: true,
-//     },
+    paket: {
+        required: true,
+    },
 
-//     isPerwakilan: {
-//         required: true,
-//     },
+    isPerwakilan: {
+        required: true,
+    },
 
-//     namaPerwakilan: {
-//         type: String,
-//         required: true,
-//     },
+    namaPerwakilan: {
+        type: String,
+        required: true,
+    },
 
-//     buktiBayarNormal: {
-//         type: String,
-//         required: true,
-//     },
+    buktiBayarNormal: {
+        type: String,
+        required: true,
+    },
 
-//     buktiPersyaratanNormal: {
-//         type: String,
-//         required: true,
-//     },
-// });
+    buktiPersyaratanNormal: {
+        type: String,
+        required: true,
+    },
+});
 
-// const Normal =
-//     mongoose.models.Normal ||
-//     mongoose.model("Normal", normalSchema);
+const Normal =
+    mongoose.models.Normal ||
+    mongoose.model("Normal", normalSchema);
 
-// export default Normal;
+export default Normal;
