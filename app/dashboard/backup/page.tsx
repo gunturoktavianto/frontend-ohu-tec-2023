4a5892f8d3e17415d9089b0604377317d4140ab9
Guntur Oktavianto Nugroho <gunturoktaviant@gmail.com>
2023-09-03 09:48:15 +0700

    edit folder backup

---
+++ app/dashboard/backup/page.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+// import FormInternship from "../../component-ohu/FormInternship";
+
+export default function page() {
+    return <h1 className="text-3xl text-center font-bold">Nothing Here</h1>;
+}
--- app/dashboard/internship-form/page.tsx
+++
@@ -1,6 +0,0 @@
-// import React from "react";
-// import FormInternship from "../../component-ohu/FormInternship";
-
-// export default function page() {
-//     return <FormInternship></FormInternship>;
-// }
