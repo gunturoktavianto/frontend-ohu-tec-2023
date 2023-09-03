7df67c9f0dab56fa4fbaae02dbabdb767ee43f37
Vincentius7704 <vincentiusbhatara04@gmail.com>
2023-09-03 20:08:44 +0700

    Update Keterangan pendaftaran

--- app/component-ohu/FormInternshipDaful.tsx
+++ app/component-ohu/FormInternshipDaful.tsx
@@ -162,6 +162,7 @@ export default function FormInternshipDaful() {
                         <label htmlFor="buktiBayar">
                             Link GDrive Bukti Pembayaran
                         </label>
+                        <p className="text-xs text-slate-500">Silakan membuat folder pada Google Drive sesuai instruksi pada Guidebook Pendaftaran. Jika kamu membayar melalui cash, silakan isi kolom dengan tulisan "Cash". Kami akan mencocokkan data kamu!</p>
                         <input
                             onChange={(e) => setBuktiBayar(e.target.value)}
                             value={buktiBayar}
@@ -176,6 +177,7 @@ export default function FormInternshipDaful() {
                         <label htmlFor="buktiPersyaratan">
                             Link GDrive Bukti Persyaratan
                         </label>
+                        <p className="text-xs text-slate-500"> Pisahkan folder antara bukti pembayaran dan bukti persyaratan. Tata cara penamaan dan lainnya dapat dilihat pada Guidebook Pendaftaran.  Manggatt 😍</p>
                         <input
                             onChange={(e) =>
                                 setBuktiPersyaratan(e.target.value)
